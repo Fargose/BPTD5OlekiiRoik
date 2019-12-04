f1b7ccb3ad01b0e4dc0bd4a33af0fefffbce5f73
Oleksii Roik <oleksii.roik.pz.2016@lpnu.ua>
2019-12-04 21:34:10 +0200

    Add project files.

---
+++ BPTD5/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BPTD5
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessageTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoadFromFileButton = new System.Windows.Forms.Button();
+            this.SignatureTextBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoadKey = new System.Windows.Forms.Button();
+            this.GenerateKeyButton = new System.Windows.Forms.Button();
+            this.SaveSignatureToFile = new System.Windows.Forms.Button();
+            this.VerifySignButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(12, 67);
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(576, 358);
+            this.MessageTextBox.TabIndex = 0;
+            this.MessageTextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть текст або";
+            // 
+            // LoadFromFileButton
+            // 
+            this.LoadFromFileButton.Location = new System.Drawing.Point(203, 34);
+            this.LoadFromFileButton.Name = "LoadFromFileButton";
+            this.LoadFromFileButton.Size = new System.Drawing.Size(192, 28);
+            this.LoadFromFileButton.TabIndex = 2;
+            this.LoadFromFileButton.Text = "Завантажити з файлу";
+            this.LoadFromFileButton.UseVisualStyleBackColor = true;
+            this.LoadFromFileButton.Click += new System.EventHandler(this.LoadFromFileButton_Click);
+            // 
+            // SignatureTextBox
+            // 
+            this.SignatureTextBox.Location = new System.Drawing.Point(669, 68);
+            this.SignatureTextBox.Name = "SignatureTextBox";
+            this.SignatureTextBox.Size = new System.Drawing.Size(431, 96);
+            this.SignatureTextBox.TabIndex = 3;
+            this.SignatureTextBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(664, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Підпис";
+            // 
+            // LoadKey
+            // 
+            this.LoadKey.Location = new System.Drawing.Point(744, 31);
+            this.LoadKey.Name = "LoadKey";
+            this.LoadKey.Size = new System.Drawing.Size(184, 31);
+            this.LoadKey.TabIndex = 5;
+            this.LoadKey.Text = "Завантажити з файлу";
+            this.LoadKey.UseVisualStyleBackColor = true;
+            this.LoadKey.Click += new System.EventHandler(this.LoadKey_Click);
+            // 
+            // GenerateKeyButton
+            // 
+            this.GenerateKeyButton.Location = new System.Drawing.Point(430, 33);
+            this.GenerateKeyButton.Name = "GenerateKeyButton";
+            this.GenerateKeyButton.Size = new System.Drawing.Size(158, 28);
+            this.GenerateKeyButton.TabIndex = 6;
+            this.GenerateKeyButton.Text = "Згенерувати підпис";
+            this.GenerateKeyButton.UseVisualStyleBackColor = true;
+            this.GenerateKeyButton.Click += new System.EventHandler(this.GenerateKeyButton_Click);
+            // 
+            // SaveSignatureToFile
+            // 
+            this.SaveSignatureToFile.Location = new System.Drawing.Point(934, 31);
+            this.SaveSignatureToFile.Name = "SaveSignatureToFile";
+            this.SaveSignatureToFile.Size = new System.Drawing.Size(166, 31);
+            this.SaveSignatureToFile.TabIndex = 7;
+            this.SaveSignatureToFile.Text = "Зберегти в файл";
+            this.SaveSignatureToFile.UseVisualStyleBackColor = true;
+            this.SaveSignatureToFile.Click += new System.EventHandler(this.SaveSignatureToFile_Click);
+            // 
+            // VerifySignButton
+            // 
+            this.VerifySignButton.Location = new System.Drawing.Point(193, 431);
+            this.VerifySignButton.Name = "VerifySignButton";
+            this.VerifySignButton.Size = new System.Drawing.Size(192, 73);
+            this.VerifySignButton.TabIndex = 8;
+            this.VerifySignButton.Text = "Перевірити підпис";
+            this.VerifySignButton.UseVisualStyleBackColor = true;
+            this.VerifySignButton.Click += new System.EventHandler(this.VerifySignButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 515);
+            this.Controls.Add(this.VerifySignButton);
+            this.Controls.Add(this.SaveSignatureToFile);
+            this.Controls.Add(this.GenerateKeyButton);
+            this.Controls.Add(this.LoadKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SignatureTextBox);
+            this.Controls.Add(this.LoadFromFileButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MessageTextBox);
+            this.Name = "MainForm";
+            this.Text = "DSS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox MessageTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LoadFromFileButton;
+        private System.Windows.Forms.RichTextBox SignatureTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoadKey;
+        private System.Windows.Forms.Button GenerateKeyButton;
+        private System.Windows.Forms.Button SaveSignatureToFile;
+        private System.Windows.Forms.Button VerifySignButton;
+    }
+}
+
---
+++ BPTD5/MainForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BPTD5
+{
+    public partial class MainForm : Form
+    {
+
+        private DSAParameters _privateKey;
+        private DSAParameters _publicKey;
+        public string Message { get; set; }
+
+        private byte[] hash;
+        private byte[] signedHash;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFromFileButton_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            var filePath = openFileDialog.FileName;
+            Message = System.IO.File.ReadAllText(filePath);
+            MessageTextBox.Text = Message;
+        }
+
+
+        private void GenerateKeyButton_Click(object sender, EventArgs e)
+        {
+            using (var dsa = new DSACryptoServiceProvider())
+            {
+                _privateKey = dsa.ExportParameters(true);
+                _publicKey = dsa.ExportParameters(false);
+            }
+            Message = MessageTextBox.Text;
+            byte[] byteMessage = Encoding.UTF8.GetBytes(Message);
+            hash = (new SHA1Managed()).ComputeHash(byteMessage);
+            signedHash = DsaSignHash(hash, _privateKey, "SHA1");
+            var stringBuilder = new StringBuilder();
+            foreach (byte t in signedHash)
+            {
+                stringBuilder.Append(t.ToString("x2"));
+            }
+
+            SignatureTextBox.Text = stringBuilder.ToString();
+
+
+        }
+
+        private byte[] DsaSignHash(byte[] hash, DSAParameters dsaKey, string hashAlg)
+        {
+            byte[] signature = null;
+
+            try
+            {
+                using (var dsa = new DSACryptoServiceProvider())
+                {
+                    dsa.ImportParameters(dsaKey);
+                    var dsaFormatter = new DSASignatureFormatter(dsa);
+                    dsaFormatter.SetHashAlgorithm(hashAlg);
+                    signature = dsaFormatter.CreateSignature(hash);
+                }
+            }
+            catch (CryptographicException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return signature;
+        }
+
+        private void VerifySignButton_Click(object sender, EventArgs e)
+        {
+            Message = MessageTextBox.Text;
+            byte[] byteMessage = Encoding.UTF8.GetBytes(Message);
+            hash = (new SHA1Managed()).ComputeHash(byteMessage);
+            string currentSignature = SignatureTextBox.Text;
+            byte[] currentSignatureByte = StringToByteArray(currentSignature);
+            
+            var verified = DsaVerifyHash(hash, currentSignatureByte, _publicKey, "SHA1");
+            MessageBox.Show(verified ? "Підпис дійсний!" : "Підпис недійсний!");
+        }
+
+        private bool DsaVerifyHash(byte[] hashValue, byte[] signedHashValue, DSAParameters dsaKey, string hashAlg)
+        {
+            var verified = false;
+            try
+            {
+                using (var dsa = new DSACryptoServiceProvider())
+                {
+                    dsa.ImportParameters(dsaKey);
+                    var dsaDeformatter = new DSASignatureDeformatter(dsa);
+                    dsaDeformatter.SetHashAlgorithm(hashAlg);
+                    verified = dsaDeformatter.VerifySignature(hashValue, signedHashValue);
+                }
+            }
+            catch (CryptographicException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return verified;
+        }
+
+        private byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        private void SaveSignatureToFile_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            var dialogResult = saveFileDialog.ShowDialog();
+            if (dialogResult != DialogResult.OK) return;
+            var filePath = saveFileDialog.FileName;
+
+            try
+            {
+                string currentSignature = SignatureTextBox.Text;
+                byte[] currentSignatureByte = StringToByteArray(currentSignature);
+                //var publicKEY_Dialog = new SaveFileDialog { FileName = _fileName };
+                File.WriteAllBytes(filePath, currentSignatureByte);
+            }
+            catch
+            {
+                MessageBox.Show(@"Помилка при записы в файл", @"Можливо файл використовуэться системою", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void LoadKey_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+            var loadFileDialog = new OpenFileDialog();
+            loadFileDialog.ShowDialog();
+            if (loadFileDialog.FileName != null)
+            {
+                filePath = loadFileDialog.FileName;
+            }
+            signedHash = File.ReadAllBytes(filePath);
+            var sBuilder = new StringBuilder();
+            foreach (byte t in signedHash)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+
+            SignatureTextBox.Text = sBuilder.ToString();
+        }
+    }
+}
